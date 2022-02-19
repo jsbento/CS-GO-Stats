5297960aec83f19f4453660dcd60d6ba867ca9a0
jsbento <benton_jacob@yahoo.com>
2022-02-19 16:58:02 -0600

    Fixed React error

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,11 @@
 import { Field, Form, Formik } from "formik";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Stats } from "./api/cs";
 import * as yup from "yup";
+
+interface FormValues {
+    username: string;
+}
 
 const CSNameScheme = yup.object().shape({
     username: yup.string().trim().required("Required")
@@ -9,26 +13,27 @@ const CSNameScheme = yup.object().shape({
 
 const About = () => {
     const [data, setData] = useState<Stats | null>(null);
-
+    const initialValues:FormValues = {username: ""};
     return (
         <>
-            <Formik validationSchema={CSNameScheme} initialValues={{ username: "" }} onSubmit={async (values) => {
+            <Formik validationSchema={CSNameScheme} initialValues={initialValues} onSubmit={async (values, actions) => {
                     await fetch('/api/cs', { method: 'POST', body: JSON.stringify({ username: values.username })})
                     .then((response) => response.json())
                     .then((stats) => {
                         setData(stats);
-                    });
+                    })
+                    .catch((error) => {console.log(error);});
+                    actions.setSubmitting(false);
                 }}>
-                {({errors}) => (
                     <Form>
-                        <Field name="username" type="text" placeholder="Steam account/display name/SteamID" autocomplete="off"/>
-                        {errors.username ? (<div>{errors.username}</div>):null}
+                        <label htmlFor="username">Username</label>
+                        <Field id="username" name="username" type="text" placeholder="Steam account/display name/SteamID" autoComplete="off"/>
+                        <button type='submit'>Submit</button>
                     </Form>
-                 )};
             </Formik>
-            {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+            {data && (<pre>{JSON.stringify(data, null, 2)}</pre>)}
         </>
     );
-}
+};
 
 export default About;
