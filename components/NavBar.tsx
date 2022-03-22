4690766e11538752c9770c3e8d16ef4d7e9a94a6
jsbento <benton_jacob@yahoo.com>
2022-03-21 21:29:24 -0500

    header styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ import Logo from './Logo';
 const NavBar = () => {
   return (
     <>
-        <ul className='flex justify-end p-10 gap-5 items-center'>
+        <ul className='flex justify-end p-10 gap-5 items-center bg-gray-800 text-white'>
             <li className='mr-auto'><Logo></Logo></li>
             <li className='hover:scale-105 font-bold'>
               <a href='/statistics'>Load Stats</a>
@@ -14,7 +14,7 @@ const NavBar = () => {
               <a href='/savedstats'>View Stats</a>
             </li>
             <li className='hover:scale-105 font-bold'>Item 3</li>
-            <Button>Sign In</Button>
+            <Button className='text-black'>Sign In</Button>
         </ul>
     </>
   );
