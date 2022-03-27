8d5ffa32599b82f036b670e5da169b82eafe670c
jsbento <benton_jacob@yahoo.com>
2022-03-27 18:53:58 -0500

    Limit number of stat cards rendered to 4 on client side

--- pages/statistics.tsx
+++ pages/statistics.tsx
@@ -1,5 +1,5 @@
 import { Field, Form, Formik } from "formik";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Stats } from "./api/cs";
 import * as yup from "yup";
 import StatsCard from "../components/StatsCard";
@@ -17,7 +17,7 @@ export interface ServerData {
 }
 
 const CSNameScheme = yup.object().shape({
-    username: yup.string().trim().required("Required")
+    username: yup.string().trim().required("Steam profile name required")
 });
 
 const Statistics: NextPage = () => {
@@ -57,6 +57,15 @@ const Statistics: NextPage = () => {
         .catch(error => {console.log(error);});
     };
 
+    /*
+    When users implemented
+
+    if(loggedIn)
+        return form
+    else
+        return "please log in"
+    */
+
     return (
         <div className="flex flex-col items-center">
             <Formik validationSchema={CSNameScheme} initialValues={initialValues} onSubmit={async (values, actions) => {
@@ -75,7 +84,7 @@ const Statistics: NextPage = () => {
                     {({errors, isSubmitting}) => (
                         <Form className="flex flex-col justify-center items-center">
                             <label className="font-semibold" htmlFor="username">Steam Account Name</label>
-                            <Field className="border-2" id="username" name="username" type="text" placeholder="Steam account/display name/SteamID" autoComplete="off"/>
+                            <Field className="border-2" id="username" name="username" type="text" placeholder="Steam account name" autoComplete="off"/>
                             <button className="border-2 w-auto p-1 rounded-md m-1 font-semibold" type='submit'>Fetch Stats</button>
                             {errors.username ? (<div className="text-red-600 text-lg">{errors.username}</div>) : null}
                             {isSubmitting ? (<div className="animate-pulse font-semibold text-lg">Loading...</div>) : null}
@@ -84,7 +93,7 @@ const Statistics: NextPage = () => {
             </Formik>
             {dataErr ? <p className="text-center">Error fetching data. Please use Steam account name.</p> : null}
             <div className="grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-                {dataArr && dataArr.length > 0 ? dataArr.map(data => <StatsCard {...data}/>) : null}
+                {dataArr && dataArr.length > 0 ? dataArr.slice(0, 4).map(data => <StatsCard {...data}/>) : null}
             </div>
         </div>
     );
